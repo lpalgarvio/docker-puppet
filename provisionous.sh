692b7532214d121962687c0bafa512ecd5aea513
Luís Pedro Algarvio <lp.algarvio@gmail.com>
2015-01-05 14:48:55 +0000

    Added support for Docker-Base provisioner; Refactored directory structure; Refactored up local and global variables; Fixed argument_instructions descriptions

--- provisionous.sh
+++ provisionous.sh
@@ -2,8 +2,8 @@
 #title           :provisionous.sh
 #description     :Provisionous: Operates providers and provisioners.
 #author		 :lpalgarvio <"lp.algarvio@gmail.com">
-#date            :20141231
-#version         :0.5
+#date            :20150105
+#version         :0.6
 #usage		 :bash provisionous.sh [arg1] [arg2] [arg3] [arg4]
 #notes		 :Run scripts in install directory to install the dependencies.
 #bash_version    :4.2
@@ -16,38 +16,36 @@
 # Import global configuration
 source "./config.inc";
 
-# Debugging
-if [ $debug = true ]; then
-  echo "Configuration: ${common_path}/functions.inc";
-fi;
 # Import scripts
-source "./${common_path}/functions.inc";
+source "./${functions_inc}";
+
+# Store the arguments
+declare -g args_all="$*";
+declare -g args_provisioner=$1;
+declare -g args_command=$2;
+declare -g args_distro=$3;
+declare -g args_tag=$4;
 
 # Debugging
 if [ $debug = true ]; then
-  echo "Arguments: $*";
+  echo "Arguments: ${args_all}";
 fi;
-# Store the arguments
-args="$*";
-arg_provisioner=$1;
-arg_command=$2;
-arg_distro=$3;
-arg_tag=$4;
 
 #
 # Decision-makers
 #
 
 # Validate provisioner
-case $arg_provisioner in
+case $args_provisioner in
+  # Docker-Base provisioner
+  'docker-base' )
+    # Import scripts
+    source "./${docker_inc}";
+    ;;
   # Docker-Puppet provisioner
   'docker-puppet' )
-    # Debugging
-    if [ $debug = true ]; then
-      echo "Provisioner: ${docker_puppet_path}/docker-puppet.inc";
-    fi;
     # Import scripts
-    source "./${docker_puppet_path}/docker-puppet.inc";
+    source "./${docker_inc}";
     ;;
   # Empty argument
   '' )
@@ -62,19 +60,19 @@ case $arg_provisioner in
 esac;
 
 # Validate command
-case $arg_command in
+case $args_command in
   # Clean distro/tag command
   'clean' )
     # Distro and tag provided
-    if [ -n "$arg_distro" ] && [ -n "$arg_tag" ]; then
+    if [ -n "$args_distro" ] && [ -n "$args_tag" ]; then
       # Call function to clean a tag for a distro
-      generic_clean_distro_tag $arg_distro $arg_tag;
+      generic_clean_distro_tag $args_distro $args_tag;
     # Distro provided, tag not provided
-    elif [ -n "$arg_distro" ] && [ ! -n "$arg_tag" ]; then
+    elif [ -n "$args_distro" ] && [ ! -n "$args_tag" ]; then
       # Call function to clean all tags for a distro
-      generic_clean_distro_tags $arg_distro;
+      generic_clean_distro_tags $args_distro;
     # Distro and tag not provided
-    elif [ ! -n "$arg_distro" ] && [ ! -n "$arg_tag" ]; then
+    elif [ ! -n "$args_distro" ] && [ ! -n "$args_tag" ]; then
       # Call function to clean all tags for all distros
       generic_clean_distros;
     # Invalid arguments
@@ -86,15 +84,15 @@ case $arg_command in
   # Pull distro/tag command
   'pull' )
     # Distro and tag provided
-    if [ -n "$arg_distro" ] && [ -n "$arg_tag" ]; then
+    if [ -n "$args_distro" ] && [ -n "$args_tag" ]; then
       # Call function to pull a tag for a distro
-      generic_pull_distro_tag $arg_distro $arg_tag;
+      generic_pull_distro_tag $args_distro $args_tag;
     # Distro provided, tag not provided
-    elif [ -n "$arg_distro" ] && [ ! -n "$arg_tag" ]; then
+    elif [ -n "$args_distro" ] && [ ! -n "$args_tag" ]; then
       # Call function to pull all tags for a distro
-      generic_pull_distro_tags $arg_distro;
+      generic_pull_distro_tags $args_distro;
     # Distro and tag not provided
-    elif [ ! -n "$arg_distro" ] && [ ! -n "$arg_tag" ]; then
+    elif [ ! -n "$args_distro" ] && [ ! -n "$args_tag" ]; then
       # Call function to pull all tags for all distros
       generic_pull_distros;
     # Invalid arguments
@@ -106,15 +104,15 @@ case $arg_command in
   # Build distro/tag command
   'build' )
     # Distro and tag provided
-    if [ -n "$arg_distro" ] && [ -n "$arg_tag" ]; then
+    if [ -n "$args_distro" ] && [ -n "$args_tag" ]; then
       # Call function to build a tag for a distro
-      generic_build_distro_tag $arg_distro $arg_tag;
+      generic_build_distro_tag $args_distro $args_tag;
     # Distro provided, tag not provided
-    elif [ -n "$arg_distro" ] && [ ! -n "$arg_tag" ]; then
+    elif [ -n "$args_distro" ] && [ ! -n "$args_tag" ]; then
       # Call function to build all tags for a distro
-      generic_build_distro_tags $arg_distro;
+      generic_build_distro_tags $args_distro;
     # Distro and tag not provided
-    elif [ ! -n "$arg_distro" ] && [ ! -n "$arg_tag" ]; then
+    elif [ ! -n "$args_distro" ] && [ ! -n "$args_tag" ]; then
       # Call function to build all tags for all distros
       generic_build_distros;
     # Invalid arguments
@@ -126,9 +124,9 @@ case $arg_command in
   # Run distro/tag command
   'run' )
     # Distro and tag provided
-    if [ -n "$arg_distro" ] && [ -n "$arg_tag" ]; then
+    if [ -n "$args_distro" ] && [ -n "$args_tag" ]; then
       # Call function to run a tag from a distro
-      generic_run_distro_tag $arg_distro $arg_tag;
+      generic_run_distro_tag $args_distro $args_tag;
     # Distro provided, tag not provided
     # Distro and tag not provided
     # Invalid arguments
