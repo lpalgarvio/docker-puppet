1f6ce8207dcac0e8d12286efe89358ffdce37889
Luís Pedro Algarvio <lp.algarvio@gmail.com>
2015-01-08 14:58:50 +0000

    Refactored scripts to allow custom provisions

--- provisionous.sh
+++ provisionous.sh
@@ -2,8 +2,8 @@
 #title           :provisionous.sh
 #description     :Provisionous: Operates providers and provisioners.
 #author		 :lpalgarvio <"lp.algarvio@gmail.com">
-#date            :20150105
-#version         :0.6
+#date            :20150108
+#version         :0.9
 #usage		 :bash provisionous.sh [options] {command} {provisioner} \
 #                  [distribution] [tag]
 #notes		 :Run scripts in install directory to install the dependencies.
@@ -130,15 +130,14 @@ fi;
 # Logic
 #
 
+# Call function to load provision configuration
+generic_load_provision_config $args_provisioner;
+provisioner=${provision_info['provisioner']};
+
 # Validate provisioner
-case $args_provisioner in
-  # Docker-Base provisioner
-  'docker-base' )
-    # Import scripts
-    source "./${docker_inc}";
-    ;;
-  # Docker-Puppet provisioner
-  'docker-puppet' )
+case $provisioner in
+  # Docker provisioner
+  'docker' )
     # Import scripts
     source "./${docker_inc}";
     ;;
@@ -161,7 +160,7 @@ case $args_command in
     # Distro and tag not provided
     if [ ! -n "$args_distro" ] && [ ! -n "$args_tag" ]; then
       # Call function to clean all tags for all distros
-      generic_clean_untagged;
+      generic_clean_untagged $provisioner;
     # Invalid arguments
     else
       # Call function to show error and quit
@@ -173,15 +172,15 @@ case $args_command in
     # Distro and tag provided
     if [ -n "$args_distro" ] && [ -n "$args_tag" ]; then
       # Call function to clean a tag for a distro
-      generic_clean_distro_tag $args_distro $args_tag;
+      generic_clean_distro_tag $provisioner $args_distro $args_tag;
     # Distro provided, tag not provided
     elif [ -n "$args_distro" ] && [ ! -n "$args_tag" ]; then
       # Call function to clean all tags for a distro
-      generic_clean_distro_tags $args_distro;
+      generic_clean_distro_tags $provisioner $args_distro;
     # Distro and tag not provided
     elif [ ! -n "$args_distro" ] && [ ! -n "$args_tag" ]; then
       # Call function to clean all tags for all distros
-      generic_clean_distros;
+      generic_clean_distros $provisioner;
     # Invalid arguments
     else
       # Call function to show error and quit
@@ -193,15 +192,15 @@ case $args_command in
     # Distro and tag provided
     if [ -n "$args_distro" ] && [ -n "$args_tag" ]; then
       # Call function to pull a tag for a distro
-      generic_pull_distro_tag $args_distro $args_tag;
+      generic_pull_distro_tag $provisioner $args_distro $args_tag;
     # Distro provided, tag not provided
     elif [ -n "$args_distro" ] && [ ! -n "$args_tag" ]; then
       # Call function to pull all tags for a distro
-      generic_pull_distro_tags $args_distro;
+      generic_pull_distro_tags $provisioner $args_distro;
     # Distro and tag not provided
     elif [ ! -n "$args_distro" ] && [ ! -n "$args_tag" ]; then
       # Call function to pull all tags for all distros
-      generic_pull_distros;
+      generic_pull_distros $provisioner;
     # Invalid arguments
     else
       # Call function to show error and quit
@@ -213,15 +212,15 @@ case $args_command in
     # Distro and tag provided
     if [ -n "$args_distro" ] && [ -n "$args_tag" ]; then
       # Call function to build a tag for a distro
-      generic_build_distro_tag $args_distro $args_tag;
+      generic_build_distro_tag $provisioner $args_distro $args_tag;
     # Distro provided, tag not provided
     elif [ -n "$args_distro" ] && [ ! -n "$args_tag" ]; then
       # Call function to build all tags for a distro
-      generic_build_distro_tags $args_distro;
+      generic_build_distro_tags $provisioner $args_distro;
     # Distro and tag not provided
     elif [ ! -n "$args_distro" ] && [ ! -n "$args_tag" ]; then
       # Call function to build all tags for all distros
-      generic_build_distros;
+      generic_build_distros $provisioner;
     # Invalid arguments
     else
       # Call function to show error and quit
@@ -233,7 +232,7 @@ case $args_command in
     # Distro and tag provided
     if [ -n "$args_distro" ] && [ -n "$args_tag" ]; then
       # Call function to run a tag from a distro
-      generic_run_distro_tag $args_distro $args_tag;
+      generic_run_distro_tag $provisioner $args_distro $args_tag;
     # Distro provided, tag not provided
     # Distro and tag not provided
     # Invalid arguments
